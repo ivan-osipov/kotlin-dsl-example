ecba22c71b317e370e37dc7e188989f17a568975
Ivan Osipov <i.osipov.develop@gmail.com>
2017-12-10 23:59:42 +0400

    updated demo

--- src/test/kotlin/kotlin_dsl/Demo.kt
+++ src/test/kotlin/kotlin_dsl/Demo.kt
@@ -5,55 +5,54 @@ import java.time.DayOfWeek
 import java.time.LocalTime
 
 class Demo {
+
     @Test
-    fun demo() {
-        schedule {
-            data {
-                startFrom("08:00")
+    fun `demo test`() = schedule {
+        data {
+            startFrom("08:00")
 
-                subjects("Russian",
-                        "Literature",
-                        "Algebra",
-                        "Geometry")
+            subjects("Russian",
+                    "Literature",
+                    "Algebra",
+                    "Geometry")
 
-                student {
-                    name = "Ivanov"
-                    subjectIndexes(0, 2)
+            student {
+                name = "Ivanov"
+                subjectIndexes(0, 2)
+            }
+
+            student {
+                name = "Petrov"
+                subjectIndexes(1, 3)
+            }
+
+            teacher {
+                subjectIndexes(0, 1)
+                availability {
+                    monday("08:00")
+                    wednesday("09:00", "16:00")
                 }
-
-                student {
-                    name = "Petrov"
-                    subjectIndexes(1, 3)
+            }
+            teacher {
+                subjectIndexes(2, 3)
+                availability {
+                    thursday("08:00") + sameDay("11:00") + sameDay("14:00")
                 }
+            }
 
-                teacher {
-                    subjectIndexes(0, 1)
-                    availability {
-                        monday("08:00")
-                        wednesday("09:00", "16:00")
-                    }
-                }
-                teacher {
-                    subjectIndexes(2, 3)
-                    availability {
-                        thursday("08:00") + sameDay("11:00") + sameDay("14:00")
-                    }
-                }
+            // data { } // isn't compiled here because there is scope control with @DataContextMarker
 
-                // data { } // isn't compiled here because there is scope control with @DataContextMarker
+        } assertions {
+            for ((day, lesson, student, teacher) in scheduledEvents) {
+                val teacherSchedule: Schedule = teacher.schedule
+                teacherSchedule[day, lesson] shouldNotEqual null
+                teacherSchedule[day, lesson]!!.student shouldEqual student
 
-            } assertions {
-                for ((day, lesson, student, teacher) in scheduledEvents) {
-                    val teacherSchedule: Schedule = teacher.schedule
-                    teacherSchedule[day, lesson] shouldNotEqual null
-                    teacherSchedule[day, lesson]!!.student shouldEqual student
+                val studentSchedule = student.schedule
+                studentSchedule[day, lesson] shouldNotEqual null
+                studentSchedule[day, lesson]!!.teacher shouldEqual teacher
 
-                    val studentSchedule = student.schedule
-                    studentSchedule[day, lesson] shouldNotEqual null
-                    studentSchedule[day, lesson]!!.teacher shouldEqual teacher
-
-                    println("${DayOfWeek.of(day + 1)} (${LocalTime.of(8, 0).plusHours(lesson.toLong())}) T: ${teacher.name} S: ${student.name}")
-                }
+                println("${DayOfWeek.of(day + 1)} (${LocalTime.of(8, 0).plusHours(lesson.toLong())}) T: ${teacher.name} S: ${student.name}")
             }
         }
     }
